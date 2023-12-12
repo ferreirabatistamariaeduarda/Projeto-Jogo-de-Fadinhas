b0c39e085d4b25744aae93e06e6ba4ce3b426850
Maria Eduarda <batista.eduarda62@aluno.ifce.edu.br>
2023-12-12 14:01:13 -0300

    Classe fada criada

---
+++ Fada.java
@@ -0,0 +1,25 @@
+public class Fada {
+    private String nome;
+    private int vida;
+
+    public Fada(String nome, int vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public void usarPoder(Fada alvo) {
+        
+    }
+}
