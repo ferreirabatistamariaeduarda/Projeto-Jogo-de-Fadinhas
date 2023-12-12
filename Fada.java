49cc799cf3eb1e8501dfa331227bc524ba69576d
Vicencya Nayara <vicencyan@gmail.com>
2023-12-12 16:25:08 -0300

    Codigo completo

--- Fada.java
+++ Fada.java
@@ -1,44 +1,61 @@
-// Classe base para personagem
-class Fada{
+class Fada {
     private String nome;
     private int vida;
     private Poder poder;
+    private Poder poderEspecial;
+    private boolean poderEspecialDisponivel;
 
-    // Construtor para inicializar os atributos do personagem
-    public Fada(String nome, int vida, Poder poder) {
+    public Fada(String nome, int vida, Poder poder, Poder poderEspecial) {
         this.nome = nome;
         this.vida = vida;
         this.poder = poder;
+        this.poderEspecial = poderEspecial;
+        this.poderEspecialDisponivel = true; // O poder especial está disponível no início
     }
 
-    // Método para obter o nome do personagem
     public String getNome() {
         return nome;
     }
 
-    // Método para obter a vida atual do personagem
     public int getVida() {
         return vida;
     }
 
-    // Método para ajustar a vida do personagem
     public void setVida(int vida) {
         this.vida = vida;
     }
 
-    // Método para obter o poder do personagem
     public Poder getPoder() {
         return poder;
     }
 
-    // Método para realizar um ataque ao alvo usando o poder
+    public Poder getPoderEspecial() {
+        return poderEspecial;
+    }
+
+    public boolean isPoderEspecialDisponivel() {
+        return poderEspecialDisponivel;
+    }
+
+    public void usarPoderEspecial() {
+        if (poderEspecialDisponivel) {
+            System.out.println(nome + " está usando o poder especial: " + poderEspecial);
+            // Lógica específica para o poder especial (pode ser adicionada conforme necessário)
+            poderEspecialDisponivel = false; // O poder especial foi usado e precisa recarregar
+        } else {
+            System.out.println("Poder especial de " + nome + " está recarregando. Aguarde o próximo turno.");
+        }
+    }
+
     public void atacar(Fada alvo) {
+        // Utiliza o poder normal para atacar
         int dano = poder.calcularDano();
         alvo.receberDano(dano);
         System.out.println(nome + " atacou " + alvo.getNome() + " causando " + dano + " de dano.");
+        // Após atacar, o poder especial fica disponível novamente
+        poderEspecialDisponivel = true;
     }
 
-    // Método para processar o dano recebido e ajustar a vida
     private void receberDano(int dano) {
         vida -= dano;
         if (vida < 0) {
