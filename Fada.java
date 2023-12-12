f375dddd2a2ffc815f3aec7fdc3e364b72a7f13b
Vicencya Nayara <vicencyan@gmail.com>
2023-12-12 14:52:35 -0300

    Dano e Ataque, e Classe Poder

--- Fada.java
+++ Fada.java
@@ -1,25 +1,49 @@
-public class Fada {
+// Classe base para personagem
+class Fada{
     private String nome;
     private int vida;
+    private Poder poder;
 
-    public Fada(String nome, int vida) {
+    // Construtor para inicializar os atributos do personagem
+    public Fada(String nome, int vida, Poder poder) {
         this.nome = nome;
         this.vida = vida;
+        this.poder = poder;
     }
 
+    // Método para obter o nome do personagem
     public String getNome() {
         return nome;
     }
 
+    // Método para obter a vida atual do personagem
     public int getVida() {
         return vida;
     }
 
+    // Método para ajustar a vida do personagem
     public void setVida(int vida) {
         this.vida = vida;
     }
 
-    public void usarPoder(Fada alvo) {
-        //poderes
+    // Método para obter o poder do personagem
+    public Poder getPoder() {
+        return poder;
     }
-} bhjjgh
+
+    // Método para realizar um ataque ao alvo usando o poder
+    public void atacar(Fada alvo) {
+        int dano = poder.calcularDano();
+        alvo.receberDano(dano);
+        System.out.println(nome + " atacou " + alvo.getNome() + " causando " + dano + " de dano.");
+    }
+
+    // Método para processar o dano recebido e ajustar a vida
+    private void receberDano(int dano) {
+        vida -= dano;
+        if (vida < 0) {
+            vida = 0;
+        }
+        System.out.println(nome + " recebeu " + dano + " de dano. Vida restante: " + vida);
+    }
+}
---
+++ Poder.java
@@ -0,0 +1,8 @@
+
+public class Poder {
+
+    public int calcularDano() {
+        return 0;
+    }
+
+}
