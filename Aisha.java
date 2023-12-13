452b298a5905ac9910737dde942724da956f24c2
Maria Eduarda <batista.eduarda62@aluno.ifce.edu.br>
2023-12-13 11:05:41 -0300

    main consertado

--- Aisha.java
+++ Aisha.java
@@ -1,12 +1,12 @@
 public class Aisha extends Fada {
 
-    public class Aisha extends Fada {
+    
 
         public Aisha() {
             super("Aisha", 100, new Poder("Onda Morphix", 45, 90), new Poder("Fluidos", 35, 75));
         }
     
     
-}
+
     
 }
