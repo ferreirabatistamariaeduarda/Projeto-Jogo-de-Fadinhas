46348a7fed2df6fe2781e792522607e8ea7b5948
Vicencya Nayara <vicencyan@gmail.com>
2023-12-19 13:41:36 -0300

    solu

--- Stella.java
+++ Stella.java
@@ -1,3 +1,5 @@
+//Classe Stella para herdar Fada
+
 class Stella extends Fada {
     public Stella() {
         super("Stella", 100, new Poder("Pérolas Solares", 30, 80), new Poder("Poder Estelar", 40, 85));
